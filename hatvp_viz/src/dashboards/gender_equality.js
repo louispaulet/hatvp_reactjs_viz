cf1e1234553e4f87b749d98ae321c6f9dc3c77a5
louispaulet <louispaulet13@gmail.com>
2024-06-23 23:48:35 +0200

    update dashboard design

--- hatvp_viz/src/dashboards/gender_equality.js
+++ hatvp_viz/src/dashboards/gender_equality.js
@@ -12,70 +12,77 @@ const GenderEquality = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-extrabold text-gray-900 mb-4">👩👨 Gender Equality Analysis on HATVP Data</h1>
-      <p className="text-gray-700 mb-6">
-        This dashboard shows gendered plots to reveal the existing imbalance between men and women representatives.
-        <br /> The gender equality is measured through surnames, ratio between "Mr" and "Mme" civility prefix, mandate type, and ratio of women to men per geographical region.
-      </p>
-      <div className="mb-6">
-        <label className="block mb-2 font-medium text-gray-800">
-          Choose between all declarations or only the last declaration for each unique individual:
-        </label>
-        <div className="flex items-center space-x-4">
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="full" 
-              checked={dataset === 'full'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Full Dataset</span>
-          </label>
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="latest" 
-              checked={dataset === 'latest'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Latest Dataset</span>
-          </label>
+    <div className="min-h-screen bg-gradient-to-r from-blue-300 via-gray-200 to-red-300 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl w-full space-y-12 p-12 bg-white rounded-xl shadow-lg">
+        <div className="text-center">
+          <h1 className="text-5xl font-extrabold text-gray-900 mb-4">👩👨 Gender Equality Analysis on HATVP Data</h1>
+          <p className="mt-4 text-xl text-gray-700">
+            This dashboard shows gendered plots to reveal the existing imbalance between men and women representatives.
+            <br /> The gender equality is measured through surnames, ratio between "Mr" and "Mme" civility prefix, mandate type, and ratio of women to men per geographical region.
+          </p>
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-        <div className="bg-white rounded-lg shadow p-4"><SurnameCount dataset={dataset} /></div>
-        <div className="bg-white rounded-lg shadow p-4"><TotalGenderRatio dataset={dataset} /></div>
-        <div className="bg-white rounded-lg shadow p-4"><GenderCountPerMandateType dataset={dataset} /></div>
-        <div className="bg-white rounded-lg shadow p-4"><GenderChoropleth dataset={dataset} /></div>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 font-medium text-gray-800">
-          Choose between all declarations or only the last declaration for each unique individual:
-        </label>
-        <div className="flex items-center space-x-4">
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="full" 
-              checked={dataset === 'full'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Full Dataset</span>
+
+        <div className="mb-6">
+          <label className="block mb-2 font-medium text-gray-800">
+            Choose between all declarations or only the last declaration for each unique individual:
           </label>
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="latest" 
-              checked={dataset === 'latest'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Latest Dataset</span>
+          <div className="flex items-center space-x-4">
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="full" 
+                checked={dataset === 'full'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Full Dataset</span>
+            </label>
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="latest" 
+                checked={dataset === 'latest'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Latest Dataset</span>
+            </label>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+          <div className=" rounded-lg shadow p-4"><SurnameCount dataset={dataset} /></div>
+          <div className=" rounded-lg shadow p-4"><TotalGenderRatio dataset={dataset} /></div>
+          <div className=" rounded-lg shadow p-4"><GenderCountPerMandateType dataset={dataset} /></div>
+          <div className=" rounded-lg shadow p-4"><GenderChoropleth dataset={dataset} /></div>
+        </div>
+
+        <div className="mb-6">
+          <label className="block mb-2 font-medium text-gray-800">
+            Choose between all declarations or only the last declaration for each unique individual:
           </label>
+          <div className="flex items-center space-x-4">
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="full" 
+                checked={dataset === 'full'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Full Dataset</span>
+            </label>
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="latest" 
+                checked={dataset === 'latest'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Latest Dataset</span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
--- hatvp_viz/src/dashboards/publication_rate.js
+++ hatvp_viz/src/dashboards/publication_rate.js
@@ -12,71 +12,80 @@ const PublicationRate = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-extrabold text-gray-900 mb-4">⌛️ Publication rate of HATVP declarations</h1>
-      <p className="text-gray-700 mb-6">
-        This dashboard shows date-centric plots to analyze the posting and publication rate of declarations. <br/>
-        The goal is to show how fast, or slow, the HATVP works on the submitted declarations.  <br/>  
-        We also show the document types submitted, as well as their geographical origin.   
-      </p>
-      <div className="mb-6">
-        <label className="block mb-2 font-medium text-gray-800">
-          Choose between all declarations or only the last declaration for each unique individual:
-        </label>
-        <div className="flex items-center space-x-4">
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="full" 
-              checked={dataset === 'full'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Full Dataset</span>
-          </label>
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="latest" 
-              checked={dataset === 'latest'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Latest Dataset</span>
-          </label>
+    <div className="min-h-screen bg-gradient-to-r from-blue-300 via-gray-200 to-red-300 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl w-full space-y-12 p-12 bg-white rounded-xl shadow-lg">
+        <div className="text-center">
+          <h1 className="text-5xl font-extrabold text-gray-900 mb-4">⌛️ Publication Rate of HATVP Declarations</h1>
+          <p className="mt-4 text-xl text-gray-700">
+            This dashboard shows date-centric plots to analyze the posting and publication rate of declarations.
+            <br />
+            The goal is to show how fast, or slow, the HATVP works on the submitted declarations.
+            <br />
+            We also show the document types submitted, as well as their geographical origin.
+          </p>
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-        <div className="bg-white rounded-lg shadow p-4"><DeclarationsPerDateCount dataset={dataset} /></div>
-        <div className="bg-white rounded-lg shadow p-4"><AverageDelay dataset={dataset} /></div>
-        <div className="bg-white rounded-lg shadow p-4"><DocumentTypes dataset={dataset} /></div>
-        <div className="bg-white rounded-lg shadow p-4"><PublicationsChoropleth dataset={dataset} /></div>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 font-medium text-gray-800">
-          Choose between all declarations or only the last declaration for each unique individual:
-        </label>
-        <div className="flex items-center space-x-4">
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="full" 
-              checked={dataset === 'full'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Full Dataset</span>
+
+        <div className="mb-6">
+          <label className="block mb-2 font-medium text-gray-800">
+            Choose between all declarations or only the last declaration for each unique individual:
           </label>
-          <label className="flex items-center">
-            <input 
-              type="radio" 
-              className="form-radio text-indigo-600" 
-              value="latest" 
-              checked={dataset === 'latest'} 
-              onChange={handleDatasetChange} 
-            />
-            <span className="ml-2 text-gray-800">Latest Dataset</span>
+          <div className="flex items-center space-x-4">
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="full" 
+                checked={dataset === 'full'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Full Dataset</span>
+            </label>
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="latest" 
+                checked={dataset === 'latest'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Latest Dataset</span>
+            </label>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+          <div className=" rounded-lg shadow p-4"><DeclarationsPerDateCount dataset={dataset} /></div>
+          <div className=" rounded-lg shadow p-4"><AverageDelay dataset={dataset} /></div>
+          <div className=" rounded-lg shadow p-4"><DocumentTypes dataset={dataset} /></div>
+          <div className=" rounded-lg shadow p-4"><PublicationsChoropleth dataset={dataset} /></div>
+        </div>
+
+        <div className="mb-6">
+          <label className="block mb-2 font-medium text-gray-800">
+            Choose between all declarations or only the last declaration for each unique individual:
           </label>
+          <div className="flex items-center space-x-4">
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="full" 
+                checked={dataset === 'full'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Full Dataset</span>
+            </label>
+            <label className="flex items-center">
+              <input 
+                type="radio" 
+                className="form-radio text-blue-700" 
+                value="latest" 
+                checked={dataset === 'latest'} 
+                onChange={handleDatasetChange} 
+              />
+              <span className="ml-2 text-gray-800">Latest Dataset</span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
