91b0388e53200ab3eac34417da3bb7219ecff115
louispaulet <louispaulet13@gmail.com>
2024-06-29 15:18:30 +0200

    update legends and axis display on popular employers barplot

--- hatvp_viz/src/plots/barplot_popular_employers_per_gender.js
+++ hatvp_viz/src/plots/barplot_popular_employers_per_gender.js
@@ -48,7 +48,12 @@ const PopularEmployersPerYear = () => {
   };
 
   const formatYAxis = (tickItem) => {
-    return `${(tickItem / 1000000).toFixed(0)}M€`;
+      if (currentPage === 0) {
+        return `${(tickItem / 1000000).toFixed(0)}M€`;
+      } else {
+        return `${(tickItem / 1000).toFixed(0)}k€`;
+      }
+    
   };
 
   const displayedData = data.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
@@ -58,24 +63,27 @@ const PopularEmployersPerYear = () => {
   return (
     <section className="mb-6 p-4 bg-white">
       <h2 className="text-xl font-extrabold text-gray-900 mb-4">Top 100 most popular employers</h2>
+      <p> We selected the top 100 employers by total reported revenue. <br/>
+      The employers have been selected as "popular" if at least 2 persons declared the same employer.  <br/>
+      </p>
       <ResponsiveContainer width="100%" height={400}>
         <BarChart
           width={500}
           height={300}
           data={displayedData}
           margin={{
-            top: 20, right: 30, left: 30, bottom: 20,
+            top: 20, right: 30, left: 30, bottom: 50,
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="employer">
-            <Label value="Employer" position="insideBottom" dy={10} />
+          <XAxis dataKey="employer" tick={{ angle: -45, textAnchor: 'end', fontSize: 8 }} interval={0}>
+            <Label value="Employer" position="insideBottom" dy={50} />
           </XAxis>
           <YAxis tickFormatter={formatYAxis}>
             <Label value="Total Amount" angle={270} position="insideLeft" offset={-5} dx={-10} />
           </YAxis>
           <Tooltip formatter={(value) => `${(value / 1000).toFixed(0)}k€`} />
-          <Legend />
+          <Legend verticalAlign="top" />
           <Bar dataKey="F_revenue" stackId="a" fill={COLORS.F_revenue} />
           <Bar dataKey="M_revenue" stackId="a" fill={COLORS.M_revenue} />
         </BarChart>
@@ -89,7 +97,7 @@ const PopularEmployersPerYear = () => {
           Previous
         </button>
         <span className="px-4 py-2">
-          Page {currentPage + 1}/{totalPages}
+          {currentPage + 1}/{totalPages}
         </span>
         <button
           className={`px-4 py-2 bg-blue-500 text-white rounded ${(currentPage + 1) * rowsPerPage >= data.length ? 'opacity-50 cursor-not-allowed' : ''}`}
